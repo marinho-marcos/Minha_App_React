bc1673494e009b4f4245e3fddb9ac44f88a54a2d
Marcos Marinho <marcosmarinho485@gmail.com>
2025-11-17 16:52:11 -0300

    upgrade

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,35 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
 
 const Main = () => {
   return (
-    <div>
-      <h1>Conteúdo principal</h1>
-    </div>
+    <main>
+      <Carousel>
+        <Carousel.Item>
+          <ExampleCarouselImage text="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <ExampleCarouselImage text="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <ExampleCarouselImage text="Third slide" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </main>
   )
 }
 
